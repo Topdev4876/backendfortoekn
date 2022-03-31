1a44ff699f404289b9104c0c07d5f9ab57621b9f
kirill <xldev007@gmail.com>
2022-04-01 04:17:23 +0900

    first commit

---
+++ config/database.js
@@ -0,0 +1,2 @@
+module.exports.DB_PORT =
+  process.env.DB_PORT || 'mongodb+srv://Admin:kwon4876@cluster0.abgq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
---
+++ config/system_settings.js
@@ -0,0 +1,4 @@
+module.exports = {
+  REPLY_EMAIL: 'Metamask <support@lgltrax.com>',
+  REPLY_NUMBER: '+18636941562',
+};
