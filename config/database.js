465794c7971b043835365d051845a12f13e1c000
kirill <xldev007@gmail.com>
2022-04-01 05:14:21 +0900

    add procfile and fix database link

--- config/database.js
+++ config/database.js
@@ -1,2 +1,1 @@
-module.exports.DB_PORT =
-  process.env.DB_PORT || 'mongodb+srv://Admin:kwon4876@cluster0.abgq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+module.exports.DB_PORT ='mongodb+srv://Admin:kwon4876@cluster0.abgq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
